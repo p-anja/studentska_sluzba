f89835f965f11afdce003b0db1aafd19c824ea82
p-anja <73345582+p-anja@users.noreply.github.com>
2020-11-26 10:12:33 +0100

    #menu_bar dodate ikonice, mnemonici, aceleratori

--- src/view/MenuBar.java
+++ src/view/MenuBar.java
@@ -1,11 +1,21 @@
 package view;
 
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class MenuBar extends JMenuBar{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public MenuBar() {
 		
 		JMenu file= new JMenu("File");
@@ -14,20 +24,36 @@ public class MenuBar extends JMenuBar{
 		
 		JMenuItem miNew = new JMenuItem("New");
 		JMenuItem miClose = new JMenuItem("Close");
-		miNew.setIcon(new ImageIcon("images/buttonplus.png"));
+		miNew.setIcon(new ImageIcon("images/new.png"));
+		miNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		miNew.setMnemonic(KeyEvent.VK_N);
 		file.add(miNew);
 		file.addSeparator();
 		miClose.setIcon(new ImageIcon("images/close.png"));
+		miClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+		miClose.setMnemonic(KeyEvent.VK_C);
 		file.add(miClose);
 
 		JMenuItem miEdit = new JMenuItem("Edit");
 		JMenuItem miDelete = new JMenuItem("Delete");
+		miEdit.setIcon(new ImageIcon("images/edit.png"));
+		miEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		miEdit.setMnemonic(KeyEvent.VK_E);
+		miDelete.setIcon(new ImageIcon("images/delete.png"));
+		miDelete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+		miDelete.setMnemonic(KeyEvent.VK_D);
 		edit.add(miEdit);
 		edit.addSeparator();
 		edit.add(miDelete);
 
 		JMenuItem miHelp = new JMenuItem("Help");
 		JMenuItem miAbout = new JMenuItem("About");
+		miHelp.setIcon(new ImageIcon("images/help.png"));
+		miHelp.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
+		miHelp.setMnemonic(KeyEvent.VK_H);
+		miAbout.setIcon(new ImageIcon("images/about.png"));
+		miAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+		miAbout.setMnemonic(KeyEvent.VK_A);
 		help.add(miHelp);
 		help.addSeparator();
 		help.add(miAbout);
