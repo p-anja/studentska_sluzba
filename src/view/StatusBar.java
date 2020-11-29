fa2a7b81da9150286ca110d8677b2e0663df073f
p-anja <73345582+p-anja@users.noreply.github.com>
2020-11-29 20:59:59 +0100

    #status_bar promenjena boja

--- src/view/StatusBar.java
+++ src/view/StatusBar.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -11,15 +12,20 @@ import javax.swing.JPanel;
 
 public class StatusBar extends JPanel implements Runnable{
 	
-	private Thread thread;
-	private JLabel time;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	Thread thread = new Thread();;
+	JLabel time = new JLabel();
 
 	@Override
 	public void run() {
 		try {
 			do {
-				DateFormat data = new SimpleDateFormat("hh:mm   dd.MM.yyyy.     ");
-				Calendar calendar = Calendar.getInstance();
+				DateFormat data = new SimpleDateFormat("HH:mm   dd.MM.yyyy.     ");
+				Calendar calendar = Calendar.getInstance(); // Returns instance with current date and time set
 				time.setText(data.format(calendar.getTime()));
 				Thread.sleep(1000);
 			} while (thread.isAlive());
@@ -31,6 +37,7 @@ public class StatusBar extends JPanel implements Runnable{
 	public StatusBar() {
 		
 		setPreferredSize(new Dimension(100, 30));
+		setBackground(Color.PINK);
 		setLayout(new BorderLayout());
 		
 		JLabel title = new JLabel("     Studentska sluzba");
