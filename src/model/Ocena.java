61a3ec8b2e089dd531b87944bc3f6a41641458e1
p-anja <73345582+p-anja@users.noreply.github.com>
2020-12-23 10:43:17 +0100

    #ocena kreirana klasa modela

---
+++ src/model/Ocena.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Ocena {
+
+	private Student student;
+	private Predmet predmet;
+	private int vrednostOcene;
+	private String datumPolaganja;
+	
+	public Ocena(Student student, Predmet predmet, int vrednostOcene, String datumPolaganja) {
+		super();
+		this.student = student;
+		this.predmet = predmet;
+		this.vrednostOcene = vrednostOcene;
+		this.datumPolaganja = datumPolaganja;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Predmet getPredmet() {
+		return predmet;
+	}
+
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+
+	public int getVrednostOcene() {
+		return vrednostOcene;
+	}
+
+	public void setVrednostOcene(int vrednostOcene) {
+		this.vrednostOcene = vrednostOcene;
+	}
+
+	public String getDatumPolaganja() {
+		return datumPolaganja;
+	}
+
+	public void setDatumPolaganja(String datumPolaganja) {
+		this.datumPolaganja = datumPolaganja;
+	}
+	
+	
+	
+}
