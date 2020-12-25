430e4138e873f46a51a0f748ca217dc9133ad3b8
p-anja <73345582+p-anja@users.noreply.github.com>
2020-12-25 14:31:48 +0100

    #prikaz_studenata implementirana fukncija za pocetno popunjavanje baze

--- src/model/StudentBase.java
+++ src/model/StudentBase.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
+
 public class StudentBase {
 	
 	private static StudentBase instance = null;
@@ -20,8 +22,11 @@ public class StudentBase {
 	
 	private StudentBase() {
 		
+		
 		this.cols = new ArrayList<String>();
 		this.students = new ArrayList<Student>();
+		
+		initStudents();
 		
 		this.cols.add("INDEX");
 		this.cols.add("IME");
@@ -136,6 +141,16 @@ public class StudentBase {
 				s.setProsecnaOcena(prosecnaOcena);
 			}
 		}
+	}
+	
+	private void initStudents() {
+		this.students = new ArrayList<Student>();
+		students.add(new Student("Mika", "Mikić", "12.3.1999.", "Šumadijska 8, Novi Sad", "063563214", "mika@mail.com", "in-96-2018", "2018", 3, StatusStudent.B));
+		students.add(new Student("Pera", "Perić", "11.10.1998.", "Zlatne grede 1, Novi Sad", "065852963", "pera@mail.com", "ra-33-2017", "2017", 4, StatusStudent.S));
+		students.add(new Student("Toma", "Tomić", "20.3.2000.", "Alekse Šantića 3, Novi Sad", "063773214", "toma@mail.com", "ar-96-2019", "2019", 2, StatusStudent.B));
+		students.add(new Student("Ana", "Vidić", "20.2.2001.", "Ćirpanova 3, Novi Sad", "063721324", "ana@mail.com", "in-96-2020", "2020", 1, StatusStudent.B));
+		students.add(new Student("Jelena", "Čavić", "15.3.1999.", "Laze Kostića 2, Novi Sad", "064214564", "jelena@mail.com", "ar-180-2018", "2018", 3, StatusStudent.S));
+		students.add(new Student("Nikola", "Nikolić", "10.8.2000.", "Balzakova 13, Novi Sad", "062456987", "nikola@mail.com", "in-3-2019", "2019", 2, StatusStudent.S));
 	}
 	
 
